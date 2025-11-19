9c5bd0727db27000329b88099d0ea11f18f3c0f8
StathisKaragiorgos <efstathios.karagiorgos@gmail.com>
2025-11-19 18:57:02 +0200

    Remove node_modules files and add .gitignore

--- loginPage/src/main.jsx
+++ loginPage/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter,RouterProvider } from 'react-router'
+import Homepage from "./components/loginSignup/homepage.jsx"
+import LoginSignUp from './components/loginSignup/loginSignUp.jsx'
+
+const router=createBrowserRouter([
+  {path:"/", element:<LoginSignUp />},
+  {path:"/homepage", element:<Homepage />}
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
